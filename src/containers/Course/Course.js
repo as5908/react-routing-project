890fee1b8aaa1a9c4e0b16ccd8ccdfc2df3b6378
twmrdx <abhinavsahai01+sourcetree@gmail.com>
2020-03-27 08:36:14 -0700

    Added links and query params routing

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 
 class Course extends Component {
-    render () {
-        return (
-            <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
-            </div>
-        );
+  state = {
+    title: null,
+    id: null
+  };
+
+  componentDidMount() {
+    const search = this.props.location.search;
+    const query = new URLSearchParams(this.props.location.search);
+    for (let param of query.entries()) {
+      console.log(param); // yields ['start', '5']
+      this.setState({ [param[0]]: param[1] });
     }
+  }
+  render() {
+    console.log(this.props);
+
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+        <p>You selected the Course with ID: {this.state.id}</p>
+      </div>
+    );
+  }
 }
 
 export default Course;
